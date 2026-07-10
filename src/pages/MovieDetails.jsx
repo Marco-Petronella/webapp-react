685d7e85bc7056c4ba3987211972cd8023f71be3
Marco-Petronella <marco.petronella85@gmail.com>
2026-07-10 17:00:36 +0200

    add form to publish new reviews

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -5,6 +5,12 @@ export default function MovieDetails() {
   const [movie, setMovie] = useState([]);
   const id = useParams().id;
   const [reviews, setReviews] = useState([]);
+  const [formData, setFormData] = useState({
+    movie_id: id,
+    name: "",
+    vote: "",
+    text: "",
+  });
 
   useEffect(() => {
     fetch(`http://localhost:3000/v1/movies/${id}`)
@@ -14,6 +20,17 @@ export default function MovieDetails() {
         console.log(result);
       })
   }, [id]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch(`http://localhost:3000/v1/movies/${id}/review`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+  }
 
   return (
     <main>
@@ -40,6 +57,41 @@ export default function MovieDetails() {
       ) : (
         <p>Loading...</p>
       )}
+    <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+          />  
+          </div>
+
+          <div className="mb-3">
+          <label htmlFor="text" className="form-label">Text</label>
+          <input
+            type="text"
+            className="form-control"
+            id="text"
+            value={formData.text}
+            onChange={(e) => setFormData({ ...formData, text: e.target.value })}
+          />  
+          </div>
+
+          <div className="mb-3">
+          <label htmlFor="name" className="form-label">Vote</label>
+          <input
+            type="text"
+            className="form-control"
+            id="vote"
+            value={formData.vote}
+            onChange={(e) => setFormData({ ...formData, vote: e.target.value })}
+          />  
+          </div>
+          <button type="submit" className="btn btn-primary">Add review</button>
+      </form>
     </main>
   );
 }
