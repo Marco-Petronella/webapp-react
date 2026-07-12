c3d2e9a8e277aeab51af7e4265405b721c5cbff5
Marco-Petronella <marco.petronella85@gmail.com>
2026-07-12 12:41:35 +0200

    add validate form and stars on reviews

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -5,6 +5,7 @@ export default function MovieDetails() {
   const [movie, setMovie] = useState([]);
   const id = useParams().id;
   const [reviews, setReviews] = useState([]);
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const [formData, setFormData] = useState({
     movie_id: id,
     name: "",
@@ -12,17 +13,24 @@ export default function MovieDetails() {
     text: "",
   });
 
-  useEffect(() => {
-    fetch(`http://localhost:3000/v1/movies/${id}`)
-      .then((response) => response.json())
-      .then((result) => {
-        setMovie(result);
-        console.log(result);
-      })
-  }, [id]);
+  function resetFormData() {
+    setFormData({
+      movie_id: id,
+      name: "",
+      vote: "",
+      text: "",
+    });
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
+    if (formData.name == "") {
+      formData.name = "Anonymous";
+    }
+    if (formData.vote == "" || formData.vote < 1 || formData.vote > 5) {
+      alert("Vote must be a number between 1 and 5");
+      return;
+    }
     fetch(`http://localhost:3000/v1/movies/${id}/review`, {
       method: "POST",
       headers: {
@@ -30,7 +38,27 @@ export default function MovieDetails() {
       },
       body: JSON.stringify(formData),
     });
+    setFormSubmitted(!formSubmitted);
+    resetFormData();
   }
+
+  function drawStars(vote) {
+    const stars = [];
+    for (let i = 0; i <= 5; i++) {
+          if (i < vote) stars.push(<i key={i} className="bi bi-star-fill"></i>);
+          else stars.push(<i key={i} className="bi bi-star"></i>);
+    }
+    return stars;
+  }
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/v1/movies/${id}`)
+      .then((response) => response.json())
+      .then((result) => {
+        setMovie(result);
+        console.log(result);
+      });
+  }, [formSubmitted]);
 
   return (
     <main>
@@ -38,7 +66,11 @@ export default function MovieDetails() {
       {movie ? (
         <>
           <h2>{movie[0]?.title}</h2>
-          <img src={`/src/assets/images/${movie[0]?.image}`} alt={movie[0]?.title} />
+          <img
+            className="poster"
+            src={`/src/assets/images/${movie[0]?.image}`}
+            alt={movie[0]?.title}
+          />
           <p>{movie[0]?.director}</p>
           <p>{movie[0]?.year}</p>
           <p>{movie[0]?.abstract}</p>
@@ -48,7 +80,9 @@ export default function MovieDetails() {
             <div className="card" key={review.id}>
               <div className="card-body">
                 <h5 className="card-title">Revies by: {review.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted ">{review.vote}</h6>
+                <h6 className="card-subtitle mb-2 text-muted ">
+                  {drawStars(review.vote)}
+                </h6>
                 <p className="card-text">{review.text}</p>
               </div>
             </div>
@@ -57,40 +91,51 @@ export default function MovieDetails() {
       ) : (
         <p>Loading...</p>
       )}
-    <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="name" className="form-label">Name</label>
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
           <input
             type="text"
             className="form-control"
+            placeholder="Enter your name"
             id="name"
             value={formData.name}
             onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-          />  
-          </div>
+          />
+        </div>
 
-          <div className="mb-3">
-          <label htmlFor="text" className="form-label">Text</label>
+        <div className="mb-3">
+          <label htmlFor="text" className="form-label">
+            Text
+          </label>
           <input
             type="text"
             className="form-control"
             id="text"
+            placeholder="Enter your review"
             value={formData.text}
             onChange={(e) => setFormData({ ...formData, text: e.target.value })}
-          />  
-          </div>
+          />
+        </div>
 
-          <div className="mb-3">
-          <label htmlFor="name" className="form-label">Vote</label>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Vote
+          </label>
           <input
-            type="text"
+            type="number"
             className="form-control"
             id="vote"
+            placeholder="Enter your vote (1-5)"
             value={formData.vote}
             onChange={(e) => setFormData({ ...formData, vote: e.target.value })}
-          />  
-          </div>
-          <button type="submit" className="btn btn-primary">Add review</button>
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Add review
+        </button>
       </form>
     </main>
   );
